963351e5c33977ca75092d88cdd6660c5dc39734
Ubuntu <ubuntu@ip-172-31-20-1.ap-northeast-1.compute.internal>
2025-11-17 12:18:05 +0000

    [Add]編集機能

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get 'about' => 'homes#about'
   root to: "homes#top"
   get 'posts/:id' => 'posts#show'
+  patch 'posts/:id' => 'posts#update', as: 'update_post'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
