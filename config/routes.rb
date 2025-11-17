e26916f664af3f3d9685772ea62a7b7010abd5cd
Ubuntu <ubuntu@ip-172-31-20-1.ap-northeast-1.compute.internal>
2025-11-17 11:57:13 +0000

    [Add]詳細画面

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   get 'about' => 'homes#about'
   root to: "homes#top"
+  get 'posts/:id' => 'posts#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
