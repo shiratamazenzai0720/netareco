6e241e99dc407c1e0763b3db6e4007457c045707
Ubuntu <ubuntu@ip-172-31-20-1.ap-northeast-1.compute.internal>
2025-11-19 08:10:34 +0000

    [Add]ユーザーの退会機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
   def destroy
     user = User.find(params[:id])
     user.destroy
-    redirect_to new_user_session_path
+    redirect_to new_user_registration_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get 'about' => 'homes#about'
   resources :posts, only: [:new, :create, :index, :show, :destroy, :edit, :update]
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:show, :edit, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
