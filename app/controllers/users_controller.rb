30afe4ad68ff322fd6393b3509063de229b88ddc
Ubuntu <ubuntu@ip-172-31-20-1.ap-northeast-1.compute.internal>
2025-11-19 08:26:11 +0000

    [Update]ユーザーページの編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,6 +31,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:name, :profile_image)
+    params.require(:user).permit(:name, :profile_image, :email)
   end
 end
