f469cb1454f767fe996819a04c25f5f391b3eab9
Ubuntu <ubuntu@ip-172-31-20-1.ap-northeast-1.compute.internal>
2025-12-10 14:14:03 +0000

    [Add]コメント追加機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   validates :name, presence: true
   validates :email, presence: true
+
   has_one_attached :profile_image
   has_many :posts, dependent: :destroy
+  has_many :post_comments, dependent: :destroy
 
   def get_profile_image(width, height)
     unless profile_image.attached?
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   get 'about' => 'homes#about'
-  resources :posts, only: [:new, :create, :index, :show, :destroy, :edit, :update]
+  resources :posts, only: [:new, :create, :index, :show, :destroy, :edit, :update] do
+    resources :post_comments, only: [:create, :destroy]
+  end
   resources :users, only: [:show, :edit, :update, :destroy]
   get '/search', to: 'searches#search'
 end
