99a3d91bd890f34c3d538681a1cadeeb8eefce8c
Ubuntu <ubuntu@ip-172-31-20-1.ap-northeast-1.compute.internal>
2025-12-11 14:21:34 +0000

    [Add]管理者と顧客の使用できる機能の仕分け

---
+++ app/controllers/admin/posts_controller.rb
@@ -0,0 +1,58 @@
+class Admin::PostsController < ApplicationController
+    before_action :authenticate_admin!
+
+    def new
+      @post = Post.new
+    end
+  
+    def create
+      @post = Post.new(post_params)
+      @post.user_id = current_user.id
+      if @post.save
+        redirect_to post_path(@post.id)
+      else
+        render :new
+      end
+    end
+  
+    def index
+      @posts = Post.all
+    end
+  
+    def show
+      @post = Post.find(params[:id])
+      @post_comment = PostComment.new
+    end
+  
+    def edit
+      @post = Post.find(params[:id])
+    end
+  
+    def update
+      @post = Post.find(params[:id])
+      if @post.update(post_params)
+      redirect_to post_path(@post.id)
+      else
+        render :edit
+      end
+    end
+  
+    def destroy
+      post = Post.find(params[:id])
+      post.destroy
+      redirect_to user_path(current_user.id)
+    end
+  
+    private
+    
+    def post_params
+      params.require(:post).permit(:title,:body)
+    end
+  
+    def is_matching_login_user
+      post = Post.find(params[:id])
+      unless post.user_id == current_user.id
+        redirect_to posts_path
+      end
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,33 @@
 Rails.application.routes.draw do
-  devise_for :users
+# 管理者用
+# URL /admin/sign_in ...
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+
+#管理者側ルーディング
+namespace :admin do
   root to: "homes#top"
-  get 'about' => 'homes#about'
-  resources :posts, only: [:new, :create, :index, :show, :destroy, :edit, :update] do
-    resources :post_comments, only: [:create, :destroy]
-  end
-  resources :users, only: [:show, :edit, :update, :destroy, :index]
+  resources :posts, only: [:index, :show, :destroy, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update, :destroy]
   get '/search', to: 'searches#search'
+end
+
+# 顧客用
+# URL /users/sign_in ...
+  devise_for :users,  skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+# 顧客側ルーディング
+  scope module: :public do
+    root to: "homes#top"
+    get 'about' => 'homes#about'
+    resources :posts, only: [:new, :create, :index, :show, :destroy, :edit, :update] do
+      resources :post_comments, only: [:create, :destroy]
+    end
+    resources :users, only: [:show, :edit, :update, :destroy, :index]
+    get '/search', to: 'searches#search'
+  end
 end
